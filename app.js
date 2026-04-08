307298f5063bcf7408bd410341c374eb28cf68ca
carolinebrylling <bryllingcaroline@gmail.com>
2026-04-08 08:49:56 +0200

    mine første ændringer

---
+++ app.js
@@ -0,0 +1,7 @@
+"use strict";
+
+document.body.innerHTML += "Hello JavaScript!";
+// alert("Hello Dat.js");
+console.log("Hello Multimedia Designers");
+console.log("Hi Students!");
+console.log("This is your first JavaScript App 🎉");
